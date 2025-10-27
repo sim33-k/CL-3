096fdec7e9a726e16994e281c401b830ff21e664
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-27 20:18:23 +0530

    add starter code in the question

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,8 @@
 // given the current month and the number of periods
 // which window does the current month belong to
 
-console.log("test123");
+function getWindowIdx(noOfPeriods, currentMonth)
+{
+ //your code here
+ return window;
+}
